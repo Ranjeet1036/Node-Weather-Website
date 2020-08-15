2350095e8b6b6c91c984f6a634616591aa14b097
Ranjeet1036 <ranjeet10361@gmail.com>
2020-08-16 00:56:41 +0530

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude,callback)=>{
         }else if(response.body.error){
             callback('Unable to find location!',undefined);
         }else{
-            callback(undefined,response.body.current.weather_descriptions[0]+". It is currently "+ response.body.current.temperature +" degrees out. It feels like "+ response.body.current.feelslike +" degree out there." );
+            
+            callback(undefined,response.body.current.weather_descriptions[0]+". It is currently "+ response.body.current.temperature +" degrees out. It feels like "+ response.body.current.feelslike +" degree out there. The humidity is "+response.body.current.humidity+"%");
             
         }
     });
